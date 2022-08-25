d11cdd4ca0a375651769939f81bb18a575582de5
olayidecodes <olayidecodes@gmail.com>
2022-08-25 19:58:14 +0100

    added changes

--- src/App.js
+++ src/App.js
@@ -1,19 +1,44 @@
 import './App.css';
-import {useSelector} from 'react-redux'
+import { useState } from 'react';
+import {useSelector, useDispatch} from 'react-redux'
+import {addUser, deleteUser, updateUsername} from './features/Users'
 
 function App() {
 
+  const dispatch = useDispatch();
   const userList = useSelector((state) => state.users.value)
+
+  const [name, setName] = useState('')
+  const [username, setUsername] = useState('')
+  const [newUsername, setNewUsername] = useState('')
 
   return (
     <div className="App">
       <div className='addUser'>
-        <input type="text" name="" placeholder='Name' />
-        <input type="text" name="" placeholder='Username' />
-        <button type="">Add User</button>
+        <input type="text" placeholder='Name' onChange={(event) => {setName(event.target.value)}} />
+        <input type="text" name="" placeholder='Username' onChange={(event) => {setUsername(event.target.value)}} />
+        <button onClick={() => {dispatch(addUser({id: userList[userList.length - 1].id + 1, name, username}))}}>Add User</button>
       </div>
       <div className='displayUsers'>
-        
+        {userList.map((user) => {
+          return (
+            <div>
+              <h1>{user.name}</h1>
+              <h4>{user.username}</h4>
+              <footer>
+                <input type="text" name="" placeholder='Username' onChange={(event) => {setNewUsername(event.target.value)}} />
+                <button onClick={() => {
+                  dispatch(updateUsername({id: user.id, username: newUsername}))
+                }}>Update Username</button>
+                <button onClick={() => {
+                  dispatch(deleteUser({id: user.id}))
+                }}>Delete Username</button>
+              </footer>
+              
+            </div>
+          
+          )
+        })}
       </div>
     </div>
   );
